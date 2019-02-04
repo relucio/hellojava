8d256c575e4f705ade8abb750c1dbcddf8a6fdc0
Ike Relucio <relucio@ph.ibm.com>
2019-02-04 14:23:09 +0800

    First Commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.ibm.esr.microservice.helloworld;    // you can change the package name
+
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.util.ArrayList;
+
+
+@Path("/helloworld")  // you can change the path to your microservice
+public class HelloWorld {
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response example() {
+        return Response.ok("Hello World! you have reached host " + System.getenv("HOSTNAME")).build();
+    }
+
+
+}
+
